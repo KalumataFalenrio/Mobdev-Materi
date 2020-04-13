d00369f35a8a41c2510a4f9164900d320f25c31a
falenrio kalumata <52149548+KalumataFalenrio@users.noreply.github.com>
2020-04-13 08:46:38 +0800

    my msg

---
+++ Apps.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Text>Selamat Datang!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
